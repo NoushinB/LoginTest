86bdab5dfd676db927bb7e5aefae4943fe55fd93
Noushin <nou.babakhani@gmail.com>
2023-10-18 23:16:11 +0100

    added RunCucumberTest

--- src/test/java/noushin/RunCucumberTest.java
+++ src/test/java/noushin/RunCucumberTest.java
@@ -1,4 +1,12 @@
 package noushin;
 
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty"}, strict = true, tags = "not @todo")
 public class RunCucumberTest {
+
+
 }
