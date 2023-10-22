319e8f704980913223092bc30624c54f8cb5745b
Noushin <nou.babakhani@gmail.com>
2023-10-22 11:27:23 +0100

    Comments added to PropertyLoader

--- src/test/java/noushin/core/library/PropertyLoader.java
+++ src/test/java/noushin/core/library/PropertyLoader.java
@@ -1,11 +1,11 @@
 package noushin.core.library;
 
 
+/**
+ * A Singleton Class
+ */
 public class PropertyLoader {
     private static volatile PropertyLoader instance;
-
-    private String platform;
-    private String appPath;
 
     public static PropertyLoader getInstance() {
         if (instance == null) {
@@ -18,10 +18,28 @@ public class PropertyLoader {
         return instance;
     }
 
+    /**
+     * Property to save our platform
+     */
+    private String platform;
+
+    /**
+     * Property to save our app path
+     */
+    private String appPath;
+
+    /**
+     * platform Property getter
+     * @return the platform value
+     */
     public String getPlatform() {
         return platform;
     }
 
+    /**
+     * platform Property setter
+     * @param platform new value for platform
+     */
     public void setPlatform(String platform) {
         this.platform = platform;
     }
